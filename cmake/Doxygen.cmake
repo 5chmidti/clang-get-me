6246bf0242f796f486646991b634579414eb97b7
5chmidti <44101708+5chmidti@users.noreply.github.com>
2022-08-27 13:53:36 +0200

    fix cmake doxygen config

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -5,7 +5,8 @@ function(enable_doxygen)
     set(DOXYGEN_CALL_GRAPH YES)
     set(DOXYGEN_EXTRACT_ALL YES)
     find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/src)
+    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/tool/src
+                     ${PROJECT_SOURCE_DIR}/tool/include)
 
   endif()
 endfunction()
