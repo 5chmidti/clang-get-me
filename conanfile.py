29e7cab4fd1940f2856bfccdcb101ca82ad136b1
5chmidti <44101708+5chmidti@users.noreply.github.com>
2022-08-27 13:53:06 +0200

    bump conan dependencies

--- conanfile.py
+++ conanfile.py
@@ -2,13 +2,12 @@ from conans import ConanFile
 
 
 class TemplateProject(ConanFile):
-    options = {}
     name = "TemplateProject"
 
     requires = (
-        "fmt/8.1.1",
+        "fmt/9.0.0",
         "spdlog/1.10.0",
-        "gtest/1.11.0",
+        "gtest/1.12.1",
         "boost/1.79.0",
         "range-v3/0.12.0"
     )
