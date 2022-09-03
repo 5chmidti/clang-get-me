92e2722e0058d54b981a90e4aca34f97c5c67a17
5chmidti <44101708+5chmidti@users.noreply.github.com>
2022-09-03 14:18:35 +0200

    bump fmt to 9.1.0

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class TemplateProject(ConanFile):
     name = "TemplateProject"
 
     requires = (
-        "fmt/9.0.0",
+        "fmt/9.1.0",
         "spdlog/1.10.0",
         "gtest/1.12.1",
         "boost/1.79.0",
