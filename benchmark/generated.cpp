bc9803ce660155ecc0550cd95b1c3c5c1a5c28cb
5chmidti <44101708+5chmidti@users.noreply.github.com>
2022-10-02 12:06:57 +0200

    add simple forking benchmark

--- benchmark/generated.cpp
+++ benchmark/generated.cpp
@@ -1,6 +1,7 @@
 #include <string_view>
 
 #include <benchmark/benchmark.h>
+#include <boost/algorithm/string/replace.hpp>
 #include <fmt/ranges.h>
 #include <range/v3/range/concepts.hpp>
 #include <range/v3/view/concat.hpp>
@@ -10,8 +11,7 @@
 
 #include "get_me_benchmarks.hpp"
 
-[[nodiscard]] inline std::string generateCode() { return ""; }
-[[nodiscard]] inline std::pair<std::string, std::string>
+[[nodiscard]] static std::pair<std::string, std::string>
 generateStraightPath(const size_t Length) {
   ranges::views::iota(static_cast<size_t>(0U), Length);
   return {
@@ -22,6 +22,22 @@ generateStraightPath(const size_t Length) {
                                   return fmt::format(
                                       "struct A{0} {{}}; A{1} getA{1}(A{0});",
                                       Iter, Iter - 1);
+                                }),
+                            "\n"))};
+}
+
+[[nodiscard]] static std::pair<std::string, std::string>
+generateForkingPath(const size_t NumStructs) {
+  ranges::views::iota(static_cast<size_t>(0U), NumStructs);
+  return {
+      "A0",
+      fmt::format("struct A0{{}};\n{}",
+                  fmt::join(ranges::views::iota(size_t{0U}, NumStructs / 2) |
+                                ranges::views::transform([](const size_t Iter) {
+                                  return fmt::format(
+                                      "struct A{0} {{}}; struct A{1} {{}}; "
+                                      "A{2} getA{2}(A{0}, A{1});",
+                                      (Iter + 1) * 2 - 1, (Iter + 1) * 2, Iter);
                                 }),
                             "\n"))};
 }
@@ -60,4 +76,9 @@ void BM_generator(benchmark::State &State, T &&Generator) {
 // NOLINTNEXTLINE
 BENCHMARK_CAPTURE(BM_generator, straightPath, generateStraightPath)
     ->Range(1, size_t{1U} << size_t{12U})
+    ->Complexity();
+
+// NOLINTNEXTLINE
+BENCHMARK_CAPTURE(BM_generator, forkingPath, generateForkingPath)
+    ->DenseRange(1, 10, 2)
     ->Complexity();
