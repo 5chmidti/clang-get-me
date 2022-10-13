56261d06595cb88a3cdac813004757d2d40fcab9
5chmidti <44101708+5chmidti@users.noreply.github.com>
2022-10-13 17:37:03 +0200

    fix tempaltePath generated benchmark

--- benchmark/generated.cpp
+++ benchmark/generated.cpp
@@ -11,30 +11,26 @@ GENERATE_GENERATED_BENCHMARKS(
     forkingPath, GenerateForkingPath, ->DenseRange(1, 10, 1)->Complexity());
 
 // NOLINTNEXTLINE
-GENERATE_GENERATED_BENCHMARKS(
-    templatePath,
-    generateFromTemplate("A0", "struct A0 {};",
-                         [](const size_t Iter) -> std::string {
-                           return fmt::format(R"(
-struct D{1} {{
-  A{0} &getA{0}();
-}};
+GENERATE_GENERATED_BENCHMARKS(templatePath,
+                              generateFromTemplate("A0", "struct A0 {};",
+                                                   [](const size_t Iter) {
+                                                     return fmt::format(
+                                                         R"(
+struct A{1};
+struct B{0};
+struct C{0};
+struct D{0};
+struct E{0};
 
-struct C{1} {{
-  C{1}() = delete;
-  static C{1} create();
-}};
-
-struct B{1} {{
-  B{1}(A{0} &, C{1});
-}};
-
-struct A{1}{{ explicit A{1}(B{1}); }};
-
-A{1} getA{1}();
-B{1} getB{1}(int);
+A{0} getA{0}(B{0});
+A{0} getA{0}(C{0});
+B{0} getB{0}(D{0});
+C{0} getC{0}(D{0});
+C{0} getC{0}(E{0});
+D{0} getD{0}(A{1});
+E{0} getE{0}();
     )",
-                                              Iter, Iter + 1);
-                         }),
-        ->Range(1, size_t{1U} << size_t{10U})
-        ->Complexity());
+                                                         Iter, Iter + 1);
+                                                   }),
+                                  ->DenseRange(1, 10, 1)
+                                  ->Complexity());
