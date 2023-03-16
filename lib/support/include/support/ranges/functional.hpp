10b7636f84b763e22ebd0b3182bf7a08cb72352f
5chmidti <44101708+5chmidti@users.noreply.github.com>
2023-03-16 13:01:56 +0100

    refactor functional.hpp
    - add sensible concepts to template parameters

--- lib/support/include/support/ranges/functional.hpp
+++ lib/support/include/support/ranges/functional.hpp
@@ -2,122 +2,136 @@
 #define get_me_lib_support_include_support_ranges_functional_hpp
 
 #include <functional>
+#include <type_traits>
 #include <utility>
 
 #include <range/v3/functional/comparisons.hpp>
 #include <range/v3/range/concepts.hpp>
 
-inline constexpr auto Plus = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::plus<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Plus = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::plus<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Minus = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::minus<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Minus = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::minus<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Multiplies = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::multiplies<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Multiplies = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::multiplies<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Divides = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::divides<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Divides = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::divides<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Modulus = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::modulus<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Modulus = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::modulus<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Negate = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::negate<T>{}(Lhs, CapturedRhs);
+inline constexpr auto Negate = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::negate<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto LogicalAnd = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::logical_and<T>{}(Lhs, CapturedRhs);
+inline constexpr auto LogicalAnd = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::logical_and<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto LogicalOr = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::logical_or<T>{}(Lhs, CapturedRhs);
+inline constexpr auto LogicalOr = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::logical_or<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto LogicalNot = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::logical_not<T>{}(Lhs, CapturedRhs);
+inline constexpr auto LogicalNot = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::logical_not<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto BitAnd = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::bit_and<T>{}(Lhs, CapturedRhs);
+inline constexpr auto BitAnd = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::bit_and<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto BitOr = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::bit_or<T>{}(Lhs, CapturedRhs);
+inline constexpr auto BitOr = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::bit_or<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto BitXor = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::bit_xor<T>{}(Lhs, CapturedRhs);
+inline constexpr auto BitXor = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::bit_xor<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto BitNot = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return std::bit_not<T>{}(Lhs, CapturedRhs);
+inline constexpr auto BitNot = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>(const U &Lhs) constexpr {
+    return std::bit_not<T>{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto EqualTo = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::equal_to{}(Lhs, CapturedRhs);
+inline constexpr auto EqualTo =
+    []<std::equality_comparable T>(T Rhs) constexpr {
+      return [Rhs]<std::regular U>
+        requires std::equality_comparable_with<U, T>(const U &Lhs)
+      constexpr {
+        return ranges::equal_to{}(Lhs, Rhs);
+      };
+    };
+
+inline constexpr auto NotEqualTo = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>
+    requires std::equality_comparable_with<U, T>(const U &Lhs)
+  constexpr {
+    return ranges::not_equal_to{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto NotEqualTo = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::not_equal_to{}(Lhs, CapturedRhs);
+inline constexpr auto Greater = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>
+    requires std::totally_ordered_with<U, T>(const U &Lhs)
+  constexpr {
+    return ranges::greater{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Greater = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::greater{}(Lhs, CapturedRhs);
+inline constexpr auto Less = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>
+    requires std::totally_ordered_with<U, T>(const U &Lhs)
+  constexpr {
+    return ranges::less{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto Less = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::less{}(Lhs, CapturedRhs);
+inline constexpr auto GreaterEqual = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>
+    requires std::totally_ordered_with<U, T>(const U &Lhs)
+  constexpr {
+    return ranges::greater_equal{}(Lhs, Rhs);
   };
 };
 
-inline constexpr auto GreaterEqual = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::greater_equal{}(Lhs, CapturedRhs);
-  };
-};
-
-inline constexpr auto LessEqual = []<typename T>(T &&Rhs) constexpr {
-  return [CapturedRhs = std::forward<T>(Rhs)](const auto &Lhs) constexpr {
-    return ranges::less_equal{}(Lhs, CapturedRhs);
+inline constexpr auto LessEqual = []<std::regular T>(T Rhs) constexpr {
+  return [Rhs]<std::regular U>
+    requires std::totally_ordered_with<U, T>(const U &Lhs)
+  constexpr {
+    return ranges::less_equal{}(Lhs, Rhs);
   };
 };
 
@@ -141,13 +155,17 @@ inline constexpr auto Lookup =
            std::is_lvalue_reference_v<T>
 {
   if constexpr (std::is_const_v<T>) {
-    return [&LookupRangeRef, Proj]<typename IndexType>(
-               const IndexType &Index) constexpr -> decltype(auto) {
+    return [&LookupRangeRef, Proj ]<typename IndexType>
+      requires std::regular_invocable<Projection, IndexType>(
+                   const IndexType &Index)
+    constexpr->decltype(auto) {
       return LookupRangeRef[std::invoke(Proj, Index)];
     };
   } else {
-    return [&LookupRangeRef, Proj]<typename IndexType>(
-               const IndexType &Index) constexpr mutable -> decltype(auto) {
+    return [&LookupRangeRef, Proj ]<typename IndexType>
+      requires std::regular_invocable<Projection, IndexType>(
+                   const IndexType &Index)
+    constexpr mutable->decltype(auto) {
       return LookupRangeRef[std::invoke(Proj, Index)];
     };
   }
