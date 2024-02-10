d7dc761213c5a3f19cf934982a3f6e8c7a4d53cb
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-02-10 16:58:33 +0100

    fix dir path passed to doxygen

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -5,8 +5,8 @@ function(enable_doxygen)
     set(DOXYGEN_CALL_GRAPH YES)
     set(DOXYGEN_EXTRACT_ALL YES)
     find_package(Doxygen REQUIRED dot)
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/tool/src
-                     ${PROJECT_SOURCE_DIR}/tool/include)
+    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR}/lib/
+                     ${PROJECT_SOURCE_DIR}/tool/)
 
   endif()
 endfunction()
