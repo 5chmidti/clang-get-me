163086944ba7a6ec31152f9df3cced2553ce8e92
5chmidti <44101708+5chmidti@users.noreply.github.com>
2024-03-24 02:55:22 +0100

    update cmake.yml GitHub workflow
    - use conan to invoke the build and test

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,11 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
 
 
 class GetMe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps"
 
     def configure(self):
         self.options["boost"].without_atomic = True
@@ -29,6 +30,9 @@ class GetMe(ConanFile):
         self.options["boost"].without_type_erasure = True
         self.options["boost"].without_wave = True
 
+        # Fixes build failure in CI
+        self.options["onetbb"].tbbbind = False
+
     def requirements(self):
         self.requires("benchmark/1.8.3")
         self.requires("boost/1.84.0")
@@ -43,3 +47,16 @@ class GetMe(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        cmake = CMake(self)
+        cmake.test(target="check")
